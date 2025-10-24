e93b6bf5f78349a95d32ccbf8675027f320f640a
b-elia <bordonielia96@gmail.com>
2025-10-25 01:16:02 +0200

    'complete deploy script'

--- script/DeployDSC.s.sol
+++ script/DeployDSC.s.sol
@@ -4,16 +4,24 @@ pragma solidity ^0.8.13;
 import {Script} from "forge-std/Script.sol";
 import {DSCEngine}from "../src/DSCEngine.sol"; 
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
+import {HelperConfig} from "script/HelperConfig.sol";
 
 
-contract CounterScript is Script {
+contract DeployDSC is Script {
    
-    function setUp() public {}
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
 
     function run() public returns(DecentralizedStableCoin, DSCEngine){
+        HelperConfig helperConfig = new HelperConfig();
+        (address weth, address wbtc, address wethUsdPriceFeed, address wbtcUsdPriceFeed, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
         vm.startBroadcast();
         DecentralizedStableCoin dsc = new DecentralizedStableCoin();
-        DSCEngine engine = new DSCEngine();
+        DSCEngine engine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsc));
+        dsc.transferOwnership(address(engine));
         vm.stopBroadcast();
     }
 }
